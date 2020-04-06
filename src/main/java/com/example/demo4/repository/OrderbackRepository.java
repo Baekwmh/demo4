354d6c8508fa36e3e9427c91dbb3b709f2b69019
王梦慧 <2502981524@qq.com>
2020-04-06 20:47:13 +0800

    1234

---
+++ src/main/java/com/example/demo4/repository/OrderbackRepository.java
@@ -0,0 +1,7 @@
+  package com.example.demo4.repository;
+
+import com.example.demo4.entity.Orderback;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderbackRepository extends JpaRepository<Orderback,Integer> {
+}
